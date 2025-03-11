419a0c756a5356baa16d31d358350faafa52d3ea
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-03-11 18:28:31 -0300

    Fazendo ultima alteração

--- Program.cs
+++ Program.cs
@@ -123,7 +123,6 @@ namespace CadastroClientes
   {
     public string Nome { get; set; }
     public string Email { get; set; }
-
         public Cliente(string nome, string email)
     {
         Nome = nome;
